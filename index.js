9341075ddbcdd914090971d73f7cad87e2b6e49a
brajpatel <brajeshpatel0002@gmail.com>
2023-07-10 16:26:14 +0100

    Add appropriate links

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Let\'s go!');
+const https = require('https');
+const fs = require('fs');
+
+const server = https.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello, World</h1>');
+})
+
+server.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
+})
