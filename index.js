7d1f84b32074c7f2188fa99f3f4c4fc04979d900
brajpatel <brajeshpatel0002@gmail.com>
2023-07-20 17:53:52 +0100

    Create web server

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
-const http = require('http');
-const fs = require('fs');
+// ===== WITH EXPRESS =====
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 8080;
 
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
 
-    let path = req.url;
-    let fileName;
-    switch(path) {
-        case '/':
-            fileName = './index.html';
-            break;
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/about.html'));
+})
 
-        case '/about':
-            fileName = './about.html';
-            break;
+app.get('/contact-me', (req, res) => {
+    res.sendFile(path.join(__dirname, '/contact-me.html'));
+})
 
-        case '/contact-me':
-            fileName = './contact-me.html';
-            break;
+app.listen(port, () => console.log(`Listening on port:${port}`));
 
-        default:
-            fileName = './404.html';
-    }
+
+// ===== WITHOUT EXPRESS =====
+// const http = require('http');
+// const fs = require('fs');
+
+// http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+
+//     let path = req.url;
+//     let fileName;
+//     switch(path) {
+//         case '/':
+//             fileName = './index.html';
+//             break;
+
+//         case '/about':
+//             fileName = './about.html';
+//             break;
+
+//         case '/contact-me':
+//             fileName = './contact-me.html';
+//             break;
+
+//         default:
+//             fileName = './404.html';
+//     }
     
-    fs.readFile(fileName, (err, content) => {
-        if(err) {
-            console.error(err);
-        }
-        else {
-            res.end(content, 'utf8');
-        }
-    })
-}).listen(8080);
+//     fs.readFile(fileName, (err, content) => {
+//         if(err) {
+//             console.error(err);
+//         }
+//         else {
+//             res.end(content, 'utf8');
+//         }
+//     })
+// }).listen(8080);
